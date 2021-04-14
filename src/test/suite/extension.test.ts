1fde8780dbf0b92acb170c3ec3e226d19e81874d
Nick Miller <nickamilli@gmail.com>
2021-04-13 20:18:20 -0600

    Switch to npm run pretest while I figure out why tests only work locally

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -5,7 +5,7 @@ import * as path from "path";
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from "vscode";
-import { before, beforeEach } from "mocha";
+import { before } from "mocha";
 import { PytestFixtureCompletionItemProvider } from "../../suggestionProvider";
 
 suite("Extension Test Suite", () => {
