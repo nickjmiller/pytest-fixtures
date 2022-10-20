6d11f9e77e60322387a7dc88ace6d9e61ce024dd
donfiguerres <lyndonfiguerres@gmail.com>
2022-10-21 00:46:58 +0800

    Better way to find the end character position

--- src/suggestionProvider.ts
+++ src/suggestionProvider.ts
@@ -31,7 +31,8 @@ const positionOfOpenParens = (document: vscode.TextDocument, position: vscode.Po
     let pos = position.translate(0, -1); // Check to the left of the cursor
     while (pos.line >= 0) {
         const line = document.lineAt(pos.line).text;
-        for (let character = line.length - 1; character >= 0; character--) {
+        const end = document.lineAt(pos).range.end.character;
+        for (let character = end; character >= 0; character--) {
             const char = line.substring(character, character + 1);
             if (char === "(") {
                 return new vscode.Position(pos.line, character);
